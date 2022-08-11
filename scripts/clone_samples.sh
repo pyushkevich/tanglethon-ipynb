b10d3fdc1a729eb3cffa40c752432965eb1b8dd2
Paul Yushkevich <pyushkevich@gmail.com>
2022-08-11 13:12:33 -0400

    Updated wildcat.pytorch to my fork

--- scripts/clone_samples.sh
+++ scripts/clone_samples.sh
@@ -14,6 +14,8 @@ function usage()
     additional options:
       -P          Skip downloading patches (only gets manifest file)
       -X          Download exact size patches (slower)
+      -l <n>      Limit download to <n> random samples per class
+      -d <regex>  Drop samples with label matching regex
 USAGETEXT
 }
 
@@ -28,8 +30,8 @@ if [[ $# -lt 1 ]]; then
 fi
 
 SERVER="https://histo.itksnap.org"
-unset APIKEY TASKID OUTDIR SKIPPATCHES EXACTPATCHES
-while getopts "s:k:t:o:PX" opt; do
+unset APIKEY TASKID OUTDIR SKIPPATCHES EXACTPATCHES NMAX DROPREGEX
+while getopts "s:k:t:o:l:d:PX" opt; do
   case $opt in
 
     s) SERVER=$OPTARG;;
@@ -38,6 +40,8 @@ while getopts "s:k:t:o:PX" opt; do
     o) OUTDIR=$OPTARG;;
     P) SKIPPATCHES=1;;
     X) EXACTPATCHES=1;;
+    l) NMAX=$OPTARG;;
+    d) DROPREGEX=$OPTARG;;
     \?) echo "Unknown option $OPTARG"; exit 2;;
     :) echo "Option $OPTARG requires an argument"; exit 2;;
 
@@ -57,6 +61,7 @@ COOKIEFILE=$(mktemp /tmp/cookie.XXXXXX)
 RESPFILE=$(mktemp /tmp/response.XXXXXX)
 curl -s -c $COOKIEFILE -d api_key=$(cat $APIKEY | jq -r .api_key) \
   $SERVER/auth/api/login -o $RESPFILE
+chmod 600 $COOKIEFILE
 
 if [[ $(cat $RESPFILE | jq -r .status) != "ok" ]]; then
   echo "Login failed: $(cat $RESPFILE | jq -r .error)"
@@ -71,6 +76,24 @@ MANI_FLT="$OUTDIR/manifest.csv"
 curl -s -b $COOKIEFILE -o "$MANI_RAW" \
   $SERVER/dltrain/api/task/$TASKID/samples/manifest.csv 
 
+# Optionally process the raw file to limit to NMAX per class
+if [[ $NMAX || $DROPREGEX ]]; then
+  head -n 1 $MANI_RAW > "$OUTDIR/manifest_subset.csv"
+  awk -F, 'NR>1 {print $3}' < $MANI_RAW | sort -u > "$OUTDIR/unique_labels.csv"
+  while IFS= read -r lab; do
+    if [[ $DROPREGEX && $lab =~ $DROPREGEX ]]; then
+      continue
+    fi
+    if [[ $NMAX ]]; then
+      awk -F, -v x="$lab" '$3==x {print $0}' < $MANI_RAW | sort -R | head -n $NMAX >> "$OUTDIR/manifest_subset.csv"
+    else
+      awk -F, -v x="$lab" '$3==x {print $0}' < $MANI_RAW | sort -R >> "$OUTDIR/manifest_subset.csv"
+    fi
+    echo "Label $lab"
+  done < "$OUTDIR/unique_labels.csv" 
+  MANI_RAW="$OUTDIR/manifest_subset.csv"
+fi
+
 # Generate patches
 mkdir -p $OUTDIR/all_patches
 rm -rf $OUTDIR/all_patches/*
@@ -78,8 +101,12 @@ rm -rf $MANI_FLT
 
 while IFS= read -r line; do
 
-  # Get ID
-  id=$(echo $line | awk -F, '{print $1}')
+  # Get parts of the line
+  IFS=, read -r id slide_name label_name x y w h args <<< "$line"
+  if [[ $id == "id" ]]; then
+    echo "$line" >> "$MANI_FLT"
+    continue
+  fi
 
   # Download patch
   PATCH="$OUTDIR/all_patches/${id}.png"
@@ -91,8 +118,7 @@ while IFS= read -r line; do
   elif [[ $EXACTPATCHES -eq 1 ]]; then
 
     # Read the dimensions, in integers
-    DIMS=$(echo $line | awk -F, '{printf("%d_%d"), int($6), int($7)}')
-    PURL="$SERVER/dltrain/api/sample/${id}/0/image_${DIMS}.png"
+    PURL="$SERVER/dltrain/api/sample/${id}/0/image_${w}_${h}.png"
 
     curl -s -b $COOKIEFILE -o "$PATCH" "${PURL}" && \
       echo "downloaded $PATCH" && \
